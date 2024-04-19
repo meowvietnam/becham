9131ec893276f093c5ef37d6960304d4dc9183d8
meowvietnam <dinhhoanglong2005@gmail.com>
2024-04-19 22:39:40 +0700

    update color

--- main.js
+++ main.js
@@ -295,7 +295,7 @@ void main(){
   //White core
   col += 10.0*vec3(smoothstep(0.003, 0.001, dist));
   //Blue glow
-  col += glow * vec3(0.1,0.4,1.0);
+  col += glow * vec3(1.0,0.05,0.3);
         
 	//Tone mapping
 	col = 1.0 - exp(-col);
